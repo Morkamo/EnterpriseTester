371cfadd02821f504b4a038071f2a98c7f9bfc67
Kirill Drozdov <131317714+Morkamo@users.noreply.github.com>
2026-09-07 09:15:48 +0900

    Initial commit

---
+++ src/main/java/ru/morkamo/enterprisetester/EnterpriseTesterApplication.java
@@ -0,0 +1,13 @@
+package ru.morkamo.enterprisetester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseTesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/morkamo/enterprisetester/EnterpriseTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.morkamo.enterprisetester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
